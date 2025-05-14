a2f0a74eac83952b0cb56f55592c4b3a5db5c6d1
CalenCritchfield <critchfieldcalen0@gmail.com>
2025-05-14 19:35:45 +0200

    Refactor PWA support: update meta tags, streamline service worker, and unify icon references

--- sw.js
+++ sw.js
@@ -1,29 +1,22 @@
 const CACHE_NAME = "shopping-list-v1";
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/styles.css",
-  "/script.js",
-  "/icons/icon-192.png",
-  "/icons/icon-512.png",
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./script.js",
+  "./images/shopping.png",
 ];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      // Cache hit - return response
-      if (response) {
-        return response;
-      }
-      return fetch(event.request);
-    })
+    caches
+      .match(event.request)
+      .then((response) => response || fetch(event.request))
   );
 });
